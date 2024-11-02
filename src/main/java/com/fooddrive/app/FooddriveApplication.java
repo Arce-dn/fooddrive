e274d3d8c709561b12cdbbccba2b45d3cd0f71dc
Arce-dn <113496236+Arce-dn@users.noreply.github.com>
2024-11-01 23:20:03 -0600

    Initial commit

---
+++ src/main/java/com/fooddrive/app/FooddriveApplication.java
@@ -0,0 +1,13 @@
+package com.fooddrive.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooddriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooddriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fooddrive/app/FooddriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fooddrive.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooddriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

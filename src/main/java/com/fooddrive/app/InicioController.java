5c9253bec6420c48b991e7bb8efcee3c5d547dd9
Arce-dn <113496236+Arce-dn@users.noreply.github.com>
2024-12-08 21:08:14 -0600

    Titulos y vistas

--- src/main/java/com/fooddrive/app/InicioController.java
+++ src/main/java/com/fooddrive/app/InicioController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class InicioController {
 
     @GetMapping({"/Inicio", "/"})
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("titulo", "FOOD-DRIVE");
         return "home";
     }
 
